c5945457441117ebbcde18f5f07a0245f350430f
Spencer Newton <sn.newton92@gmail.com>
2020-10-18 00:38:08 +0100

    homepage styling WIP

--- src/components/page-head.jsx
+++ src/components/page-head.jsx
@@ -4,10 +4,11 @@ import Head from 'next/head';
 export default ({ title, description }) => {
   return (
     <Head>
-      {title && <meta name='title' content={title} />}
-      {title && <meta name='og:title' content={title} />}
-      {description && <meta name='description' content={description} />}
-      {description && <meta name='og:description' content={description} />}
+      {title && <meta name='title' content={title} key='title'/>}
+      {title && <meta name='og:title' content={title} key='og:title'/>}
+      {description && <meta name='description' content={description} key='description'/>}
+      {description && <meta name='og:description' content={description} key='og:description'/>}
+      <title>{title}</title>
     </Head>
   );
 };
--- src/pages/index.js
+++ src/pages/index.js
@@ -13,65 +13,45 @@ export default () => {
   return (
     <Fragment>
       <PageHead {...headerProps} />
-      <section className='hero is-dark is-fullheight-with-navbar'>
+      <section className='hero is-fullheight-with-navbar'>
         <div className='hero-body'>
-          <div className='container is-max-desktop'>
+          <div className='container is-max-desktop has-text-light'>
             <div className='has-text-right'>
-              <h1 className='hero-title title'>
+              <h1 className='hero-title has-text-weight-medium'>
                 Spencer Newton
               </h1>
             </div>
             <hr className='hero-divider' />
             <div className='hero-content'>
               <div className='columns is-vcentered reverse-columns'>
-                <div className='column is-half-tablet is-two-fifths-desktop is-half-mobile'>
+                <div className='column is-one-third-tablet is-half-mobile'>
                   <img src='images/react-icon.svg' />
                 </div>
-                <div className='column is-three-fifths-desktop'>
+                <div className='column is-two-thirds-tablet'>
                   <div className='has-text-left'>
                     <p className='has-text-justified-mobile'>👋 Hello there! I&apos;m a Software Developer currently living in Leeds. Feel free to check out the contents of this site, which includes a CV, a portfolio-of-sorts, a blog that I&apos;ll probably never update, and some secret areas you might be able to find ( ͡° ͜ʖ ͡°)</p>
                   </div>
                 </div>
               </div>
+
               <hr className='hero-divider' />
-            </div>
-          </div>
-        </div>
-      </section>
-      <section className='section is-theme-color-2 py-0'>
-        <div className='container is-max-desktop'>
-          <div className='columns is-vcentered is-centered'>
 
-            {/* About */}
-            <div className='column is-narrow'>
-              <div className='card'>
-                <div className='card-image'>
-                  <figure className='image is-16by9'>
-                    <img src='/images/merry-2.jpg' alt='merry' />
-                  </figure>
+              <div className='columns is-vcentered is-centered'>
+                <div className='column is-half'>
+                  <div className='box'>
+                    <h1 className='title'>About</h1>
+                    <h2 className='subtitle'>
+                      aboooooot meeeeeee
+                    </h2>
+                  </div>
                 </div>
-                <div className='card-content'>
-                  <h1 className='title'>About</h1>
-                  <h2 className='subtitle'>
-                    aboooooot meeeeeee
-                  </h2>
-                </div>
-              </div>
-            </div>
-
-            {/* Portfolio */}
-            <div className='column is-narrow has-text-right'>
-              <div className='card'>
-                <div className='card-image'>
-                  <figure className='image is-16by9'>
-                    <img src='/images/merry-1.jpg' alt='merry' />
-                  </figure>
-                </div>
-                <div className='card-content'>
-                  <h1 className='title'>Portfolio</h1>
-                  <h2 className='subtitle'>
-                    my stoopid apps
-                  </h2>
+                <div className='column is-half has-text-right'>
+                  <div className='box'>
+                    <h1 className='title'>Portfolio</h1>
+                    <h2 className='subtitle'>
+                      my stoopid apps
+                    </h2>
+                  </div>
                 </div>
               </div>
             </div>
