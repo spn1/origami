7d513f5dc5f5b41cd004764407904b274a79cc79
Spencer Newton <sn.newton92@gmail.com>
2021-01-24 12:35:12 +0000

    navbar burger menu for mobile devices

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,8 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Link from 'next/link';
+import classnames from 'classnames';
 import { FontAwesomeIcon as Icon } from '@fortawesome/react-fontawesome';
 
+const NavbarItem = ({ href, text }) => (
+  <div className='navbar-item is-uppercase has-text-weight-bold'>
+    <Link href={href}>
+      <a className='has-text-white'>{text}</a>
+    </Link>
+  </div>
+);
+
+const NavbarBurger = ({ isActive, onClick }) => (
+  <a role='button' onClick={onClick} className={classnames('navbar-burger', { 'is-active': isActive })} data-target='navMenu' aria-label='menu' aria-expanded='false'>
+    <span aria-hidden='true'></span>
+    <span aria-hidden='true'></span>
+    <span aria-hidden='true'></span>
+  </a>
+);
+
 export default () => {
+  const [isMobileMenuActive, setIsMobileMenuActive] = useState(false);
+
+  const burgerOnClick = () => { console.log('clicked borgar'); setIsMobileMenuActive(!isMobileMenuActive); };
+
   return (
     <nav className='navbar is-fixed-top is-theme-color-1' role='navigation' aria-label='main navigation'>
       <div className='container px-3'>
@@ -14,12 +35,13 @@ export default () => {
               </a>
             </Link>
           </div>
+          <NavbarBurger onClick={burgerOnClick} isActive={isMobileMenuActive} />
         </div>
-        <div className='navbar-menu'>
+
+        <div className={classnames('navbar-menu', 'is-theme-color-1', { 'is-active': isMobileMenuActive})}>
           <div className='navbar-end'>
-            <div className='navbar-item is-uppercase has-text-weight-bold'><Link href='/about'><a className='has-text-white'>about</a></Link></div>
-            <div className='navbar-item is-uppercase has-text-weight-bold'><Link href='/portfolio'><a className='has-text-white'>portfolio</a></Link></div>
-            {/* <div className='navbar-item is-uppercase has-text-weight-bold'><Link href='/playground/css'><a className='has-text-white'>css</a></Link></div> */}
+            <NavbarItem href='/about' text='about' />
+            <NavbarItem href='/portfolio' text='portfolio' />
           </div>
         </div>
       </div>
